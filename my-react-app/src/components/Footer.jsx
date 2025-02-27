81a290cde23e66c7a1b04e006e564f7d5b94279b
rxymitchy <lolitia15@gmail.com>
2025-02-27 22:49:02 +0300

    update react app

---
+++ my-react-app/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <footer className='footer'>&copy; 2025 Full Stack Class</footer>
+    </div>
+  )
+}
+
+export default Footer
---
+++ my-react-app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function header() {
+  return (
+    <div>
+      <h1 className='header'>MY REACT PLP CLASS</h1>
+    </div>
+  )
+}
+
+export default header
